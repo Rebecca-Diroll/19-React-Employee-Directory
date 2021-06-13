9a35784b98a93006c93bc3ccb5b71c8411e1abc4
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-06-12 22:39:20 -0400

    Add Card component and begin index.js in it

---
+++ src/components/Card/index.js
@@ -0,0 +1,6 @@
+
+import React, { Component } from 'react';
+import EmployeeCard from '../EmployeeCard';
+import Search from '../Search';
+import Header from '../Header';
+import API from '../../utils/API';
