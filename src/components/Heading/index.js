185e443f020d5c2a408880eb799facddf37d1a99
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-06-14 09:48:08 -0400

    Update index.js in Search component

--- src/components/Heading/index.js
+++ src/components/Heading/index.js
@@ -1,2 +1,13 @@
 import React from "react";
 import "./style.css";
+
+const Heading = () => {
+    return (
+        <header>
+        <h1 className='text-center'>Employee Directory</h1>
+        <h6 className='text-center'>Select column title to sort the results.</h6>
+        </header>
+    );
+};
+
+export default Heading;
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,2 +1,19 @@
 import React from "react";
 import "./style.css";
+
+const Search = (props) => {
+    return (
+        <nav className='navbar navbar-light justify-content-center'>
+            <form className='form-inline m-2'>
+                <input className='form-control'
+                    value={props.search}
+                    name='search'
+                    onChange={props.handleInputChange}
+                    type='text'
+                    placeholder='Search' />
+            </form>
+        </nav>
+    );
+};
+
+export default Search;
