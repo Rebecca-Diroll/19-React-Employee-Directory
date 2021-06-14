595bab531d19b743d0adb731d9f067a83798fc59
Rebecca Diroll <rebecca.diroll@gmail.com>
2021-06-14 09:57:56 -0400

    Update API.js in utils

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,3 +1,10 @@
 import axios from "axios";
 
 const apiURL = "https://randomuser.me/api/?results=30&nat=us";
+
+const api = {
+    getEmployees: function () {
+        return axios.get(apiURL);
+}}
+
+export default api;
